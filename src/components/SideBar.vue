b0ee6f085c6da665e5bff5116aa1303d371f7f58
Adriel Werlich <awebdev6@gmail.com>
2024-03-31 10:42:17 -0300

    remove unsued list items

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -15,9 +15,9 @@
           ></v-btn> </template
       ></v-list-item>
       <v-divider></v-divider>
-      <v-list-item link title="List Item 1"></v-list-item>
+      <!-- <v-list-item link title="List Item 1"></v-list-item>
       <v-list-item link title="List Item 2"></v-list-item>
-      <v-list-item link title="List Item 3"></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item> -->
     </v-list>
   </v-navigation-drawer>
 </template>
